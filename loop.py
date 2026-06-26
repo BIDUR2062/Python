320282093a4f7ced949b7cf10785127bfe02df5a
Bidur Khanal <bidurkhanal111@gmail.com>
2026-06-26 16:29:51 +0545

    Loop and number guessing

--- loop.py
+++ loop.py
@@ -69,3 +69,4 @@ i=1
 while(i<=10):
     print(i)
     i=i+1
+
---
+++ number_guessing_game.py
@@ -0,0 +1,31 @@
+import random
+random_num=random.randint(1,10)
+count = 0
+lives = 5
+while True:
+    if count > lives - 1:
+        print("no attempt left")
+        user_input = input("do you want to play again y/n ").lower()
+        if user_input == "y":
+            random_num = random.randint(1, 10)
+            print("random number is ", random_num)
+            count = 0
+        else:
+            print("Thank you playing")
+            break
+    print(f"Remaining attempt", lives - count)
+    count = count + 1
+    user = int(input("enter a number"))
+    if user == random_num:
+        print("guessed successfully")
+        print("attempt", count)
+        user_input = input("do you want to play again y/n ").lower()
+        if user_input == "y":
+            random_num = random.randint(1, 10)
+            print("random number is ", random_num)
+            count = 0
+        else:
+            print("Thank you playing")
+            break
+    else:
+        print("Try Again")
